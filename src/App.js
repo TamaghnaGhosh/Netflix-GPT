9f4e66195e3cebd3e4b4108ee52f11d49ade1ea5
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2023-12-19 11:46:55 +0530

    cra & tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-center text-red-500">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
