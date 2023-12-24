2db40f257b0ebd922084840e85eb02b9656ebb17
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2023-12-24 11:35:13 +0530

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,12 @@
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Header = () => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
   const User = useSelector((store) => store.user);
-
-  // What script should be run before every deploy? (npm ci && npm run build)
-  // netflixgpt-ac9da
-  // https://accounts.google.com/o/oauth2/auth?client_id=563584335869-fgrhgmd47bqnekij5i8b5pr03ho849e6.apps.googleusercontent.com&scope=email%20openid%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloudplatformprojects.readonly%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Ffirebase%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloud-platform&response_type=code&state=685393554&redirect_uri=http%3A%2F%2Flocalhost%3A9005
   
   console.log(User);
 
@@ -33,7 +30,7 @@ const Header = () => {
         alt="logo"
       />
 
-      {User && (
+      {pathname !== "/" && User && (
         <div className="flex p-2">
           <img
             className="w-12 rounded-md h-12"
