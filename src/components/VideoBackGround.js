da3bb197ecd0cf6a136bc39c6c27bf0d070c4490
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2023-12-30 17:09:58 +0530

    Tailwind classes to make MainContainer look awesome && Embedded the youtube trailer video and make it autoplay & mute

--- src/components/VideoBackGround.js
+++ src/components/VideoBackGround.js
@@ -7,9 +7,8 @@ const VideoBackground = ({ movieId }) => {
   return (
     <div>
       <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/" + movieInfo?.key}
+        className="w-screen aspect-video"
+        src={"https://www.youtube.com/embed/" + movieInfo?.key + "?autoplay=1&mute=1"}
         title="YouTube video player"
         // frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
-      <h1 className="text-3xl font-semibold">{title}</h1>
+    <div className="w-screen aspect-video pt-[20%] px-12 absolute text-white bg-gradient-to-r from-black">
+      <h1 className="text-6xl font-semibold">{title}</h1>
       <p className="py-6 text-lg w-1/4">{overview}</p>
       <div className="">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button className=" bg-white hover:bg-opacity-50 hover:text-white text-black py-4 px-12 text-xl rounded-lg">
           Watch
         </button>
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 p-2 m-2 rounded">
+        <button className="mx-2 bg-gray-500 hover:bg-gray-500 text-white py-4 px-12 text-xl bg-opacity-50 rounded-lg">
           More info
         </button>
       </div>
