ec0c1dc818f00e461172353ab886cc0978f500ad
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2024-01-26 12:09:04 +0530

    REACT_APP_FIREBASE_API_KEY

--- src/utils/services/firebase.js
+++ src/utils/services/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAhwqUsj34vre-oYtoHhKOGbvxmTWcGAzs",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "netflixgpt-ac9da.firebaseapp.com",
   projectId: "netflixgpt-ac9da",
   storageBucket: "netflixgpt-ac9da.appspot.com",
